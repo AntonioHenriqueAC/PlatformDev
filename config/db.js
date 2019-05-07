596038ec0a65c2929b295e361fe7349b3bc0859f
Antonio Henrique Cardoso <antoniohenriqueac@gmail.com>
2019-05-06 23:46:12 -0300

    segundo

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURL');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true
+        });
+
+        console.log('MongoDB connected...!!!  =))')
+    } catch (err) {
+        console.error(err.message);
+
+        // exit process with failure
+        process.exit(1);
+
+        console.log('MongoDB NOT connected...! =((')
+
+    }
+}
+
+module.exports = connectDB;
