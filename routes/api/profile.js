aeef7a2acc21e8899fdd0a5696f6548d7ba7ea6e
Antonio Henrique Cardoso <antoniohenriqueac@gmail.com>
2019-06-30 22:52:12 -0300

    experience router

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -43,12 +43,8 @@ router.post(
    [
       auth,
       [
-         check('status', 'Status is required')
-         .not()
-         .isEmpty(),
-         check('skills', 'Skills is required')
-         .not()
-         .isEmpty()
+         check('status', 'Status is required').not().isEmpty(),
+         check('skills', 'Skills is required').not().isEmpty()
       ]
    ],
    async (req, res) => {
@@ -200,68 +196,101 @@ router.delete(
       }
    });
 
-// // @route   PUT api/profile/experience
-// // @desc    Add profile experience
-// // @access  private
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  private
 
-// router.put(
-//    '/experience',
-//    // validation
-//    [
-//       auth,
-//       [
-//          check('title', 'Title is required').not().isEmpty,
-//          check('company', 'Company is required').not().isEmpty,
-//          check('from', 'From date is required').not().isEmpty
-//       ]
-//    ],
-//    async (req, res) => {
-//       const errors = validationResult(req);
-//       if (!errors.isEmpty()) {
-//          return res.status(400).json({
-//             errors: errors.array()
-//          });
-//       }
+router.put(
+   '/experience',
+   // validation
+   [
+      auth,
+      [
+         check('title', 'Title is required').not().isEmpty(),
+         check('company', 'Company is required').not().isEmpty(),
+         check('from', 'From date is required').not().isEmpty()
+      ]
+   ],
+   async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+         return res.status(400).json({
+            errors: errors.array()
+         });
+      }
 
-//       // creating field variable
-//       const {
-//          title,
-//          company,
-//          location,
-//          from,
-//          current,
-//          to,
-//          description
-//       } = req.body;
+      // creating field variable
+      const {
+         title,
+         company,
+         location,
+         from,
+         current,
+         to,
+         description
+      } = req.body;
 
-//       // creating object
-//       const newExp = {
-//          title,
-//          company,
-//          location,
-//          from,
-//          current,
-//          to,
-//          description
-//       }
+      // creating object
+      const newExp = {
+         title,
+         company,
+         location,
+         from,
+         current,
+         to,
+         description
+      };
 
-//       // code to execute
-//       try {
-//          const profile = await Profile.findOne({
-//             user: req.user.id
-//          });
+      // code to execute
+      try {
+         const profile = await Profile.findOne({
+            user: req.user.id
+         });
 
-//          profile.experience.unshift(newExp);
+         profile.experiences.unshift(newExp);
 
-//          await profile.save();
+         await profile.save();
 
-//          res.json(profile);
+         res.json(profile);
 
-//       } catch (err) {
-//          console.error(err.message);
-//          res.status(500).send('Server error!!');
-//       }
+      } catch (err) {
+         console.error(err.message);
+         res.status(500).send('Server error');
+      }
 
-//    });
+   });
+
+
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  private
+
+router.delete(
+   '/experience/:exp_id',
+   auth,
+   async (req, res) => {
+
+      // code to execute
+      try {
+         const profile = await Profile.findOne({
+            user: req.user.id
+         });
+
+         // get remove index
+         const removeIndex = profile.experiences.map(item => item.id).indexOf(req.params.exp_id);
+
+         profile.experiences.splice(removeIndex, 1);
+
+         await profile.save();
+
+         res.json(profile);
+
+      } catch (err) {
+         console.error(err.message);
+         res.status(500).send('Server error');
+      }
+
+   });
 
 module.exports = router;
